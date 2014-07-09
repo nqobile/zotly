b87135ce42c58a45ad6ae0a3a361bd969e48a8d9
nqobile <istephen@uci.edu>
2014-07-08 23:07:28 -0700

    Testing git out.

--- src/url.py
+++ src/url.py
@@ -3,9 +3,9 @@ url.py is a wrapper for all http events and methods that are related
 """
 __author__ = 'Ian Stephenson'
 
+
 import urllib.request
 
 
 def download_url(url_to_open):
-    # urllib.request.openurl() returns a bytes object w/ iso-8859-1 charset encoding
-    return urllib.request.urlopen(url_to_open).read()
+	return urllib.request.urlopen(url_to_open).read()  # urllib.request.openurl() returns a bytes object w/ iso-8859-1 charset encoding
