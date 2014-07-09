5e5e5a6511ff18b185eea6f95ec38f85091639de
nqobile <istephen@uci.edu>
2014-07-09 00:08:27 -0700

    Added course.py
    Created the Course and Class objects. A Course object holds information
    about the course along with a list of Class  instances for the course.

--- src/url.py
+++ src/url.py
@@ -1,11 +1,11 @@
 """
-url.py is a wrapper for all http events and methods that are related
+url.py is a wrapper for all http events and related methods
 """
 __author__ = 'Ian Stephenson'
-
 
 import urllib.request
 
 
 def download_url(url_to_open):
-	return urllib.request.urlopen(url_to_open).read()  # urllib.request.openurl() returns a bytes object w/ iso-8859-1 charset encoding
+    # urllib.request.openurl() returns a bytes object w/ iso-8859-1 charset encoding
+    return urllib.request.urlopen(url_to_open).read()
