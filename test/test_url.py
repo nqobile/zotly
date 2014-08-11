a1144f932ca599aeb44b1fc71be6e173416816da
nqobile <istephen@uci.edu>
2014-08-11 01:42:49 -0700

    Fixed typo from 'YearTer' to 'YearTerm' and that solved the problem. Tests are passing 100% and code works as expected.

--- test/test_url.py
+++ test/test_url.py
@@ -4,7 +4,6 @@ test_url.py is the test library correpsonding to the url.py file.
 __author__ = 'Ian Stephenson'
 
 import os
-import re
 import sys
 import unittest
 import http.client
@@ -16,7 +15,7 @@ import src.url
 class URLTestSuite(unittest.TestCase):
     response = None
     websoc_form_data = {'Submit':           'Display Web Results',
-                        'YearTer':          '2014-92',
+                        'YearTerm':          '2014-92',
                         'ShowComments':     'on',
                         'ShowFinals':       'on',
                         'Breadth':          'ANY',
@@ -46,11 +45,11 @@ class URLTestSuite(unittest.TestCase):
         self.assertIsNotNone(URLTestSuite.response)
         self.assertIsInstance(URLTestSuite.response, http.client.HTTPResponse)
         self.assertEqual(URLTestSuite.response.getcode(), 200)
-        self.assertIsNotNone(re.match('<!DOCTYPE\s+html', response_data))  # Make sure it returns HTML
+        self.assertIsNotNone('<!DOCTYPE html', response_data)  # Make sure it returns HTML
 
     def test_posturl_retrieves_class_listings(self):
         response_data = URLTestSuite.response.read().decode('utf-8')
-        self.assertIsNotNone(re.search('<div\s+class="course-list">', response_data))
+        self.assertIn('<div class="course-list">', response_data)
 
 
 if __name__ == '__main__':
