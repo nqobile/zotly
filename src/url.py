5b0b3a4bb82baeff8e4932bb5d8b10d4abe8eb11
nqobile <istephen@uci.edu>
2014-08-10 19:23:19 -0700

    Figuring out how to download a HTTP response containing the data sent from a POST request.

--- src/url.py
+++ src/url.py
@@ -1,11 +1,8 @@
-"""
-url.py is a wrapper for all http events and related methods
-"""
 __author__ = 'Ian Stephenson'
 
-import urllib.request
+
+websoc_url = 'http://websoc.reg.uci.edu/perl/WebSoc'
 
 
-def download_url(url_to_open):
-    # urllib.request.openurl() returns a bytes object w/ iso-8859-1 charset encoding
-    return urllib.request.urlopen(url_to_open).read()
+def geturl():
+    pass
--- test/test_url.py
+++ test/test_url.py
@@ -4,29 +4,48 @@ test_url.py is the test library correpsonding to the url.py file.
 __author__ = 'Ian Stephenson'
 
 import unittest
-import urllib.error
-
-from src import url
+import urllib.request
+import urllib.parse
 
 
 class URLTestSuite(unittest.TestCase):
+    websoc_url = 'http://websoc.reg.uci.edu/perl/WebSoc'
+    websoc_form_data = {'Submit':           'Display Web Results',
+                        'YearTer':          '2014-92',
+                        'ShowComments':     'on',
+                        'ShowFinals':       'on',
+                        'Breadth':          'ANY',
+                        'Dept':             'I&C SCI',
+                        'CourseNum':        '',
+                        'Division':         'ANY',
+                        'CourseCodes':      '',
+                        'InstrName':        '',
+                        'CourseTitle':      '',
+                        'ClassType':        'ALL',
+                        '&Units':           '',
+                        'Days':             '',
+                        'StartTime':        '',
+                        'EndTime':          '',
+                        'MaxCap':           '',
+                        'FullCourses':      'ANY',
+                        'FontSize':         '100',
+                        'CancelledCourses': 'Exclude',
+                        'Bldg':             '',
+                        'Room':             ''}
+    request = None
+    respose = None
 
-	def test_download_url_raises_exception_for_invalid_url(self):
-		self.assertRaises(urllib.error.URLError, url.download_url, 'hppt://g.o.o.g.l.e..c.o.m.')
+    def setUp(self):
+        urlencoded_websoc_form_data = urllib.parse.urlencode(self.websoc_form_data)
+        binary_websoc_form_data = urlencoded_websoc_form_data.encode('utf-8')
+        self.request = urllib.request.Request(self.websoc_url, data=binary_websoc_form_data, method='POST')
+        self.response = urllib.request.urlopen(self.request)
 
-	def test_download_url_returns_an_html_document(self):
-		websoc_html = url.download_url('http://websoc.reg.uci.edu/perl/WebSoc')
-		self.assertEqual(websoc_html[10:14].decode('iso-8859-1'), 'html')  # Tests the DOCTYPE header to be html
-
-	def test_download_url_returns_a_bytes_object(self):
-		websoc_html = url.download_url('http://websoc.reg.uci.edu/perl/WebSoc')
-		self.assertIsInstance(websoc_html, bytes)
-
-	def test_download_url_bytes_object_is_encoded_in_iso_8859_1(self):
-		websoc_html = url.download_url('http://websoc.reg.uci.edu/perl/WebSoc')
-		websoc_html.decode('iso-8859-1')  # This test should throw a UnicodeDecodeError if the encoding is not iso-8859-1
-		self.assertEquals
-
+    def test_geturl_sends_valid_request(self):
+        self.assertIsNotNone(self.request)
+        self.assertIsNotNone(self.response)
+        r = self.response.read().decode('utf-8')
+        print(r)
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
